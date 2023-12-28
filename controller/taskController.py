c3bda2fdd5cd798cae8df94d5fad5a304ff050d9
sudan94 <sudanupadhaya@gmail.com>
2023-12-28 16:11:49 +0100

    Added get task by id

--- controller/taskController.py
+++ controller/taskController.py
@@ -1,22 +1,17 @@
 from sqlalchemy.orm import Session
 
-from models import taskModel, userModel
+from models import taskModel
 from schemas import taskSchema
 
 def get_tasks(db: Session, skip: int =0, limit:  int = 100):
     return db.query(taskModel.Task).offset(skip).limit(limit).all()
+
+def get_task(db : Session, task_id: int):
+    return db.query(taskModel.Task).filter(taskModel.Task.id == task_id).first()
 
 def create_task(db: Session, task:taskSchema.TaskCreate):
     db_task = taskModel.Task(title = task.title, description = task.description)
     db.add(db_task)
     db.commit()
     db.refresh(db_task)
-    # create_user_task(db_task)
     return db_task
-
-# def create_user_task(db: Session, data:taskModel):
-#     db_task = userModel.UserTask(task_id = data.id)
-#     db.add(db_task)
-#     db.commit()
-#     db.refresh(db_task)
-#     return db_task
--- routers/task.py
+++ routers/task.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from schemas import taskSchema
 from controller import taskController
@@ -8,6 +8,12 @@ router = APIRouter(prefix="/tasks",
     tags=["Tasks"],
     responses={404: {"description": "Not found"}},
 )
+@router.get("/{task_id}", response_model=taskSchema.Task)
+def get_task_by_id(task_id : int, db: Session = Depends(get_db)):
+    db_task = taskController.get_task(db, task_id=task_id)
+    if db_task is None:
+        raise HTTPException(status_code=404, detail="Task not Found")
+    return db_task
 
 @router.get("", response_model=list[taskSchema.Task])
 def read_tasks(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
